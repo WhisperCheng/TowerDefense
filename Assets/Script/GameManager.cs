f8240ec92c3d40858e331339a397daf0a8a38eec
WhisperCheng <ivandominguez@sos-sistemas.com>
2024-02-19 17:03:23 +0000

    Avance8.2
    Falta todo

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -23,7 +24,7 @@ public class GameManager : MonoBehaviour
             Instance = this;
             vidasJugador = 3;
             dinero = 5000;
-            tiempoManager = 60;
+            tiempoManager = 10;
             DontDestroyOnLoad(this.gameObject);
         }
     }
@@ -34,11 +35,6 @@ public class GameManager : MonoBehaviour
     public void QuitarVidaJugador()
     {
         vidasJugador--;
-        Debug.Log(vidasJugador);
-        if (vidasJugador == 0)
-        {
-            Time.timeScale = 0;
-        }
     }
     public void AumentarDineroEnemigo()
     {
